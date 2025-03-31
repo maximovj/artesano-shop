299d741aa49e9b221c9628ae25b708f7e5fb5786
Víctor J. <maximovj@outlook.com>
2025-03-30 23:00:15 -0600

    feature: Se modifica plantilla base

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,20 +1,82 @@
 <script setup>
+import { ref, computed, defineProps } from 'vue';
 import ApplicationLogo from '@/Components/ApplicationLogo.vue';
-import { Link } from '@inertiajs/vue3';
+import { Link, usePage } from "@inertiajs/vue3";
+
+const props = defineProps({
+    showBreadcrumbs: {
+        type: Boolean,
+        default: true,
+    },
+    showCenter: {
+        type: Boolean,
+        default: false,
+    },
+    showAppBar: {
+        type: Boolean,
+        default: true,
+    }
+});
+
+const appName = usePage().props.app.name;
+
+const items = ref([]);
+items.value = window.location.pathname
+  .split("/")
+  .filter(Boolean)
+  .map((el, index, arr) => ({
+    title: el,
+    disabled: index === arr.length - 1, // El último breadcrumb está deshabilitado
+    href: "/" + arr.slice(0, index + 1).join("/")
+}));
+
+const centeredStyle = computed(() => ({
+  'min-height': props.showCenter ? '100vh' : 'auto', // Estilo dinámico
+}));
+
 </script>
 
 <template>
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-        <div>
-            <Link href="/">
-                <ApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
-            </Link>
-        </div>
+   <v-app>
+        <v-layout class="rounded rounded-md border">
+            <v-app-bar>
+                <template v-slot:prepend>
+                    <!-- Logo -->
+                    <div class="shrink-0 flex items-center">
+                        <Link :href="'/'">
+                            <ApplicationLogo
+                                class="block h-9 w-auto fill-current text-gray-800"
+                            />
+                        </Link>
+                    </div>
+                </template>
+                <Link :href="'/'" class="ml-5">
+                    <v-app-bar-title>{{ appName }}</v-app-bar-title>
+                </Link>
+                <v-spacer></v-spacer>
+                <div v-show="showAppBar">
 
-        <div
-            class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg"
-        >
-            <slot />
-        </div>
-    </div>
+                    <Link :href="route('login')" method="get">
+                        <v-btn text>Iniciar sesión</v-btn>
+                    </Link>
+                    <Link :href="route('register')" method="get">
+                        <v-btn text>Crear cuenta</v-btn>
+                    </Link>
+                </div>
+            </v-app-bar>
+
+            <v-main class="" height="300">
+                <v-container :class="{'d-flex justify-center align-center': showCenter}" :style="centeredStyle">
+                    <v-breadcrumbs v-show="showBreadcrumbs" :items="items">
+                        <template v-slot:prepend>
+                        <v-icon icon="$vuetify" size="small"></v-icon>
+                        </template>
+                    </v-breadcrumbs>
+                    
+                        <slot />
+                   
+                </v-container>
+            </v-main>
+        </v-layout>
+    </v-app>
 </template>
